f90cb9a213f307969b5a21f6e34d69b45ec9a81f
Helder Lessa <116907887+HelderLessa@users.noreply.github.com>
2023-05-16 10:13:03 -0300

    Add files via upload

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    // fetching (GET)
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch("/*WRITE YOUR DATABASE URL HERE*/");
+
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        // transforming into JSON
+        const responseData = await response.json();
+
+        // storing into an array:
+        const loadedMeals = [];
+
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            ...responseData[key],
+          });
+        }
+        // using useState to change data from array
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setHttpError(error.message);
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
